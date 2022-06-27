ddbea363993700ddb7a32c926610951f9cac03b1
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-06-27 10:59:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,23 @@ import "./../styles/App.css";
 function App() {
   const [data, setData ] = useState();
 
+
+
   
 
   const click=()=>{
     
     setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+    let x=document.querySelector('p');
+    x.setAttribute("id","para");
+    
+
    
   }
   
   return (
     <div id="main">
-      <p id="para">{data}</p>
+      <p >{data}</p>
       
       <button id="click" onClick={click}>Para</button>
     </div>
