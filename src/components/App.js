917263df6a7470325aacff5aec1f8ffe26498b60
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-06-27 10:25:58 +0530

    button working

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
+
+
 function App() {
+
+  let run=()=>{
+    document.getElementById('para').innerText="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+  }
+  
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id="para"></p>
+      
+      <button id="click" onClick={run}>para</button>
     </div>
   );
 }
