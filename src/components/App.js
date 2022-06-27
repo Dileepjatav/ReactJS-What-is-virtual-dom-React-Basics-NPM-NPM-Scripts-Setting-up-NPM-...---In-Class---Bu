addc12a2cdc755665095447354e4a5186e2c3fc7
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-06-27 10:43:52 +0530

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,21 @@ import "./../styles/App.css";
 
 
 function App() {
+  const [data, setData ] = useState();
 
-  let run=()=>{
-    document.getElementById('para').innerText="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+  
+
+  const run=()=>{
+    
+    setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+   
   }
   
   return (
     <div id="main">
-      <p id="para"></p>
+      <p id="para">{data}</p>
       
-      <button id="click" onClick={run}>para</button>
+      <button id="click" onClick={run}>Para</button>
     </div>
   );
 }
