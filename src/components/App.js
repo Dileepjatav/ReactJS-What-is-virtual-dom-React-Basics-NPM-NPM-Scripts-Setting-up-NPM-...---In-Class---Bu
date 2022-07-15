385220e4b9fac6c7608162c49737fb0f10867b4e
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-07-15 15:27:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 
-
-
 function App() {
   const [data, setData ] = useState();
-
-
-
-  
 
   const click=()=>{
     
     setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
-    
   }
   
   return (
     <div id="main">
-      <p id="para" >{data}</p>
       
+      <p id="para">{data}</p>
       <button id="click" onClick={click}>Para</button>
     </div>
   );
