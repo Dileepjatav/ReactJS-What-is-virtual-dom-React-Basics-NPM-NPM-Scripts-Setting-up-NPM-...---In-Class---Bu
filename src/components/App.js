42940fc082f343c30b6c2171b6a4ce428c80f3f3
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-07-19 19:35:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
-import React,{useState} from "react";
+import React,{Children, useState} from "react";
 import "./../styles/App.css";
 
 
 function App() {
+  
+  
   // const [data, setData ] = useState();
+
+   
+  var x=true
   
   const click=()=>{
+    
     // setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
     // let p=document.querySelector('p');
-    let p=document.querySelector('p');
-    p.innerHTML="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+
+    if(x){
+      let div=document.querySelector('#main')
+      let p=document.createElement('p')
+      p.setAttribute('id','para');
+      div.insertBefore(p,div.children[0])
+      x=false
+
+    }
+
+    
+
+    
+    
+  
+    // p.innerHTML="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
     
      
     
@@ -18,7 +38,7 @@ function App() {
   return (
     <div id="main">
       
-      <p id="para"></p>
+      {/* <p id="para">{data}</p> */}
       <button id="click" onClick={click}>Para</button>
     </div>
   );
