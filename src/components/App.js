44bf2b31bf3fc2860ceb4f14a03c783a446b4abb
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-07-19 20:12:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,21 +11,24 @@ function App() {
   var x=true
   
   const click=()=>{
+    setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
     
-    
-    // let p=document.querySelector('p');
-
     if(x){
+     
+      x=false
+      
       let div=document.querySelector('#main')
       let p=document.createElement('p')
       p.setAttribute('id','para');
       div.insertBefore(p,div.children[0])
-      x=false
+      
 
     }
     let p=document.querySelector('#para')
+    p.textContent="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+  
 
-    setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+    
     
      
     
